d7cb0a2e4ec123953685626c68dbf63816439f64
meena <kmeena0924@gmail.com>
2018-09-08 09:09:45 +0900

    rename

---
+++ Baekjoon_CodePlus_SW/src/part2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class part2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m;
+
+		m = sc.nextInt();
+		while (m-- > 0) {
+
+		}
+	}
+}
