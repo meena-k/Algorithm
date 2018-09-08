0423eda251e5186e9e3e1ac4c83a635dcde7adfc
meena <kmeena0924@gmail.com>
2018-09-08 13:15:10 +0900

    part2. 완전 탐색 0

--- Baekjoon_CodePlus_SW/src/part2/Main_10973.java
+++ Baekjoon_CodePlus_SW/src/part2/Main_10973.java
@@ -2,41 +2,37 @@ package part2;
 
 import java.util.Scanner;
 
-/* 10972. 다음 순열 */
-public class Main_10972 {
+/* 10972. 이전 순열 */
+public class Main_10973 {
 
-	public static boolean next_permutation(int[] a) {
+	public static boolean prev_permutation(int[] a) {
 		int i = a.length - 1;
-		while (i > 0 && a[i - 1] >= a[i]) {
+		while (i > 0 && a[i] >= a[i - 1])
 			i -= 1;
-		}
 
-		// 마지막 순열
 		if (i <= 0)
 			return false;
 
 		int j = a.length - 1;
-		while (a[j] <= a[i - 1])
+		while (a[j] >= a[i - 1])
 			j -= 1;
 
-		// swap
-		int temp = a[i - 1];
-		a[i - 1] = a[j];
-		a[j] = temp;
+		int temp = a[j];
+		a[j] = a[i - 1];
+		a[i - 1] = temp;
 
 		j = a.length - 1;
 		while (i < j) {
 			temp = a[i];
 			a[i] = a[j];
 			a[j] = temp;
-			i += 1;
-			j -= 1;
+			i++;
+			j--;
 		}
 		return true;
 	}
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] a = new int[n];
@@ -45,7 +41,7 @@ public class Main_10972 {
 			a[i] = sc.nextInt();
 		}
 
-		if (next_permutation(a)) {
+		if (prev_permutation(a)) {
 			for (int i = 0; i < n; i++) {
 				System.out.print(a[i] + " ");
 			}
@@ -53,6 +49,5 @@ public class Main_10972 {
 		} else {
 			System.out.println("-1");
 		}
-
 	} // end main
 } // end class
